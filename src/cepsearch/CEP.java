d9e36a511de3640f58b316361814ba93bcda97d1
unknown <alberto.abrao@fatec.sp.gov.br>
2024-03-21 11:22:16 -0300

    first insert

---
+++ src/cepsearch/CEP.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cepsearch;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class CEP {
+
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    public void buscarCep(String cep) throws MalformedURLException, IOException {
+        URL url = new URL("https://viacep.com.br/ws/" + cep + "/json/?callback=callback_name");
+        Scanner s = new Scanner(url.openStream());
+        lerAPI(s);
+
+    }
+
+    public void lerAPI(Scanner s) {
+        s.nextLine();
+        for (int i = 1; i < 10; i++) {
+            lerPalavra(s.nextLine());
+        }
+    }
+
+    public void lerPalavra(String linha) {
+        String palavra = linha.split(":")[1].replace("\"", "").replace(",", "");
+        if ("\"logradouro\"".equals(linha.split(":")[0].replace(" ", ""))) {
+            this.logradouro = palavra;
+        }
+        if ("\"bairro\"".equals(linha.split(":")[0].replace(" ", ""))) {
+            this.bairro = palavra;
+        }
+        if ("\"localidade\"".equals(linha.split(":")[0].replace(" ", ""))) {
+            this.cidade = palavra;
+        }
+        if ("\"cep\"".equals(linha.split(":")[0].replace(" ", ""))) {
+            this.cep = palavra;
+        }
+        if ("\"uf\"".equals(linha.split(":")[0].replace(" ", ""))) {
+            this.estado = palavra;
+        }
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
---
+++ src/cepsearch/CepSearch.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package cepsearch;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class CepSearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        CEP cep = new CEP();
+        cep.buscarCep("03694100");
+        System.out.println(cep.getLogradouro());
+        System.out.println(cep.getCidade());
+        System.out.println(cep.getBairro());
+        System.err.println(cep.getCep());
+        
+ 
+    }
+    
+}
