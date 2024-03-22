9824d6c79bc7580f533d0c066330c37671cd3db4
albmarques <alberto.abrao@fatec.sp.gov.br>
2024-03-22 16:02:08 -0300

    first commit

--- src/cepsearch/CepSearch.java
+++ src/cepsearch/CepSearch.java
@@ -20,7 +20,7 @@ public class CepSearch {
      */
     public static void main(String[] args) throws MalformedURLException, IOException {
         CEP cep = new CEP();
-        cep.buscarCep("03694100");
+        cep.buscarCep("55018070");
         System.out.println(cep.getLogradouro());
         System.out.println(cep.getCidade());
         System.out.println(cep.getBairro());
